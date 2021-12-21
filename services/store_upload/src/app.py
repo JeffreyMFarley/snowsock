4d94f21f9814932674f74d4e2965afbc541c078e
Jeff Farley <jeffrey.m.farley@gmail.com>
2021-12-21 14:36:12 -0500

    feat: Track missing stores

--- services/store_upload/src/app.py
+++ services/store_upload/src/app.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from random import choice
+from random import choice, randint
 from time import sleep
 
 import redis
@@ -22,20 +22,33 @@ logging.basicConfig(level = os.environ.get('LOGLEVEL', 'INFO'))
 
 stores = [x for x in range(13000)]
 
+# Remove some stores
+logger.info('Removing stores from the list')
+for _ in range(10):
+    idx = randint(0, len(stores))
+    out_of_order = stores.pop(idx)
+    logger.info(f'\t{out_of_order}')
+
 # ------------------------------------------------------
 # Main
 
 def main():
     r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
 
+    updates = 0
+    show_update_every = OPT_STORES_PER_UPDATE * 100
+
     while True:
-        logger.debug('Sending updates')
         for i in range(OPT_STORES_PER_UPDATE):
             store = choice(stores)
             logger.debug(f'Posting {store}')
             r.set(store, '<menu>')
             if OPT_PUBLISH is not None:
                 r.publish('foe', store)
+
+            updates += 1
+            if (updates % show_update_every) == 0:
+                logger.info(f'Sent {updates:>8,d} updates')
 
         sleep(OPT_SLEEP_SECONDS)
 
--- services/tlu/src/app.py
+++ services/tlu/src/app.py
@@ -11,6 +11,7 @@ import redis
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', '6379'))
 REDIS_DB = int(os.environ.get('REDIS_DB', '1'))
+OPT_MISSING_THRESHOLD = int(os.environ.get('OPT_MISSING_THRESHOLD', '120'))
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level = os.environ.get('LOGLEVEL', 'INFO'))
@@ -35,6 +36,35 @@ def monitor(redis_conn, stores):
 
 
 # ------------------------------------------------------
+# Maintain missing thread
+
+def run_check(stores, missing):
+    while True:
+        sleep(OPT_MISSING_THRESHOLD >> 2)
+
+        now = datetime.utcnow()
+
+        # Build a priority heap of the stores
+        heap = []
+        snapshot = list(stores.items())
+        for store,stamp in snapshot:
+            heapq.heappush(heap, (stamp, store))
+            if store in missing:
+                if missing[store] < stamp:
+                    logger.info(f'Store {store} updated. Removing from missing')
+                    del missing[store]
+
+        # Get the ten least updated
+        slowest = heapq.nsmallest(100, heap)
+
+        # Check if they are past the threshhold
+        for stamp, store in slowest:
+            lu = datetime.utcfromtimestamp(stamp)
+            delta = now - lu
+            if delta.total_seconds() >= OPT_MISSING_THRESHOLD:
+                missing[store] = stamp
+
+# ------------------------------------------------------
 # Main
 
 def main():
@@ -44,29 +74,32 @@ def main():
     # Set up the threads
     with mp.Manager() as manager:
         stores = manager.dict()
+        for i in range(13000):
+            stores[str(i)] = datetime.utcnow().timestamp()
 
         proc_monitor = mp.Process(target=monitor, args=(r, stores))
         proc_monitor.start()
 
+        missing = manager.dict()
+        
+        proc_missing = mp.Process(target=run_check, args=(stores, missing))
+        proc_missing.start()
+
         while True:
-            heap = []
-            snapshot = list(stores.items())
-            for store,stamp in snapshot:
-                heapq.heappush(heap, (stamp, store))
-            slowest = heapq.nsmallest(10, heap)
+            sleep(60)
 
-            watch = []
+            # Warn about missing stores
+            if len(missing):
+                now = datetime.utcnow()
 
-            now = datetime.utcnow()
-            for stamp, store in slowest:
-                lu = datetime.utcfromtimestamp(stamp)
-                delta = now - lu
-                watch.append(f'\t{store:>6s}: {delta.total_seconds():3.2f} seconds old')
-
-            logger.info('\n'.join(watch))
-            sleep(15)
+                for store, stamp in missing.copy().items():
+                    lu = datetime.utcfromtimestamp(stamp)
+                    delta = now - lu
+                    secs = delta.total_seconds()
+                    logger.warning(f'Store {store} has not responded in {secs:3.1f} seconds')
 
         proc_monitor.join()
+        proc_missing.join()
 
 
 if __name__ == '__main__':
